06d4a202a83a1d8351ef38f3be41f9aafaea4d8c
surikov <sss1024@gmail.com>
2017-04-07 16:42:24 +0300

    soundfonts

---
+++ sf2/Fluid.bat
@@ -0,0 +1,5 @@
+cd ..\sound
+java -jar D:\projects\jmuse-webaudiofont\jmuse-webaudiofont\sf2convert\sources\netbeansproject\MuseScoreSF2\dist\MuseScoreSF2.jar D:\projects\soundfont\FluidR3Mono_GM2-313.SF2 all ogg 3
+java -jar D:\projects\jmuse-webaudiofont\jmuse-webaudiofont\sf2convert\sources\netbeansproject\MuseScoreSF2\dist\MuseScoreSF2.jar D:\projects\soundfont\FluidR3Mono_GM2-313.SF2 128 ogg 0
+pause
+
