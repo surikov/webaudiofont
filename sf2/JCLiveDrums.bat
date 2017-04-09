5c100f6cf785da8c6257a6f0c543361dd0e7e01a
surikov <sss1024@gmail.com>
2017-04-09 09:13:03 +0300

    soundfonts selection

--- sf2/JCLiveDrums.bat
+++
@@ -1,4 +0,0 @@
-cd ..\sound
-java -jar D:\projects\jmuse-webaudiofont\jmuse-webaudiofont\sf2convert\sources\netbeansproject\MuseScoreSF2\dist\MuseScoreSF2.jar ..\sf2\JCLive.sf2 128 ogg 1
-pause
-
