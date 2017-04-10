b51e02b24b54d0412414263f6673865a460761c6
surikov <sss1024@gmail.com>
2017-04-10 14:55:29 +0300

    remove sounds

--- sf2/JCLive.bat
+++
@@ -1,4 +0,0 @@
-cd ../sound
-remjava -jar /home/sss/Desktop/Documents/jmuse-webaudiofont/jmuse-webaudiofont/sf2convert/executable/MuseScoreSF2.jar /home/sss/Desktop/Documents/GitHubWebAudioFont/webaudiofont/sf2/JCLive.sf2 22 ogg
-rempause
-
--- sf2/LesPaul.bat
+++
@@ -1,4 +0,0 @@
-cd ..\sound
-java -jar D:\projects\jmuse-webaudiofont\jmuse-webaudiofont\sf2convert\sources\netbeansproject\MuseScoreSF2\dist\MuseScoreSF2.jar ..\sf2\LesPaul.sf2 all ogg
-pause
-
